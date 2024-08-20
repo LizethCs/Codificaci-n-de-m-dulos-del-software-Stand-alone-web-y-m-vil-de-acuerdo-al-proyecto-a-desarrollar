354d3821eff4dbe8824548264e744ea3cbc30de6
LizethCs <lcastaeda15@soy.sena.edu.co>
2024-08-19 20:58:18 -0500

    initialize project

---
+++ src/main/java/com/evidencia/limcasoft/LimcasoftApplication.java
@@ -0,0 +1,13 @@
+package com.evidencia.limcasoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimcasoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimcasoftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evidencia/limcasoft/LimcasoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evidencia.limcasoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimcasoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

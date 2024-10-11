a9d3e64766f421a0fa1643421bdcb1195fb797f0
LizethCs <lcastaeda15@soy.sena.edu.co>
2024-10-11 18:32:21 -0500

    fix message

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# Evidencia - Codificación de módulos del software Stand alone, web y móvil de acuerdo al proyecto a desarrollar
-Realizar la codificación del módulo ```clientes``` del proyecto con el framework <strong>Spring</strong>, de acuerdo con el componente formativo “Frameworks para construcción de aplicaciones con JAVA"<br>
+# Evidencia - Codificación de módulos del software Stand 
+alone, web y móvil de acuerdo al proyecto a desarrollar
+Realizar la codificación del módulo ```clientes``` del proyecto con el framework Spring, de acuerdo con el componente formatico "“Frameworks para construcción de aplicaciones con JAVA"<br>
+
+
+https://github.com/LizethCs/Codificacion-de-modulos-del-software-Stand-alone-web-y-movil-de-acuerdo-al-proyecto-a-desarrollar
--- src/main/java/com/evidencia/limcasoft/Model/ClientModel.java
+++ src/main/java/com/evidencia/limcasoft/Model/ClientModel.java
@@ -9,6 +9,7 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.Pattern;
 
 @Entity
 @Table(name = "client")
@@ -22,12 +23,18 @@ public class ClientModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Pattern(regexp = "^[a-zA-Z]+$", message = "El nombre sólo debe contener letras")
     private String name;
+    @Pattern(regexp = "^[a-zA-Z]+ [a-zA-Z]+$", message = "El apellido no es válido.")//
     private String lastName;
+    @Pattern(regexp = "^[\\w]+([\\.\\+\\w]+)?@([\\w-]+\\.)+[a-zzA-Z]{2,}$", message = "El correo no es válido.")//subadressing +work
     private String email;
+    @Pattern(regexp = "^[a-zA-Z0-9\\s]+$", message = "La dirección no es válida.")
     private String address;
+    @Pattern(regexp = "[0-9]+$", message = "Debe contener sólo números.")
     private String number;
+    @Pattern(regexp = "^[a-zA-Z]{2,3}$", message = "Tipo de documento inválido.")
     private String docType;
+    @Pattern(regexp = "[0-9]+$", message = "Debe contener sólo números.")
     private String document;
-
 }
